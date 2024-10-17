8b496765c0662ea109974f115a31afab56e4a02c
w-duffy <w.duffy@outlook.com>
2024-10-17 03:27:30 -0500

    adds view rating and reviews specs

---
+++ tests/07-view-rating-and-reviews.spec.ts
@@ -0,0 +1,222 @@
+import { test as base, expect, chromium } from "@playwright/test";
+import {
+  signUpUser,
+  createSpot,
+  logOutUser,
+  createReview,
+  loginDemoUser,
+  createUniqueUser,
+  createSpotAndSingleReview,
+  createSpotAndMultiReviews,
+  encapsulateSpotCreation,
+  createSpotAndNoReview,
+} from "./utils";
+const test = base.extend({
+  context: async ({}, use) => {
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    await use(context);
+    await context.close();
+    await browser.close();
+  },
+  page: async ({ context }, use) => {
+    const page = await context.newPage();
+    await use(page);
+  },
+});
+test.describe("Feature: view-rating-and-reviews", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(process.env.STUDENT_URL!);
+  });
+
+  test("When viewing the home page, each spot tile in the tile list must show the average star rating for that spot immediately below the thumbnail of the tile and to the right of the spot's city & state. The average star rating should have a star icon followed by the average star rating of all the reviews for that spot as a decimal (e.g. 3.0 or 4.89, NOT 3 or 5)", async ({
+    page,
+  }) => {
+    await page.goto(process.env.STUDENT_URL!);
+    const spotTile = page.getByTestId("spot-tile").first();
+    const ratingElement = spotTile.getByTestId("spot-rating");
+    await expect(ratingElement).toBeVisible();
+    const ratingText = await ratingElement.textContent();
+    expect(
+      ratingText?.match(/New/i) || !isNaN(parseFloat(ratingText!))
+    ).toBeTruthy();
+
+    const locationElement = spotTile.getByTestId("spot-city");
+    const ratingBox = await ratingElement.boundingBox();
+    const locationBox = await locationElement.boundingBox();
+    expect(ratingBox?.y).toBeGreaterThanOrEqual(locationBox?.y!);
+    expect(ratingBox?.x).toBeGreaterThanOrEqual(locationBox?.x!);
+  });
+
+  test("When viewing a spot's detail page, the review summary info should be in two different places, the callout information box and the heading before the list of reviews. The review summary info should show the average star rating of all the reviews for that spot and the review count for that spot", async ({
+    page,
+  }) => {
+    await signUpUser(page);
+    await createSpot(page);
+    await logOutUser(page);
+    await createReview(page);
+
+    const calloutBox = page.getByTestId("spot-callout-box");
+    const reviewHeading = page.getByTestId("reviews-heading");
+
+    for (const element of [calloutBox, reviewHeading]) {
+      const ratingElement = element.getByTestId("spot-rating");
+      const reviewCountElement = element.getByTestId("review-count");
+
+      await expect(ratingElement).toBeVisible();
+      await expect(reviewCountElement).toBeVisible();
+    }
+  });
+
+  test("The average star rating in the spot's detail page should have a star icon followed by the average star rating of all the reviews for that spot as a decimal (e.g. 3.0 or 4.89, NOT 3 or 5)", async ({
+    page,
+  }) => {
+    await signUpUser(page);
+    await createSpot(page);
+    await logOutUser(page);
+    await createReview(page);
+    const calloutBox = await page.getByTestId("spot-callout-box");
+    const reviewHeading = await page.getByTestId("reviews-heading");
+
+    // TODO: Add check for star icon
+
+    for (const element of [calloutBox, reviewHeading]) {
+      const ratingText = await element.getByTestId("spot-rating").textContent();
+      expect(ratingText).toMatch(/^.\d+\.\d+$/);
+    }
+  });
+
+  test('If there are no reviews for the spot, the text, "New", should be next to the star icon instead.', async ({
+    page,
+  }) => {
+    const dummyData = createUniqueUser();
+    await encapsulateSpotCreation(page, dummyData);
+    await expect(
+      page.getByTestId("spot-callout-box").getByRole("paragraph")
+    ).toContainText("New");
+    await expect(page.getByTestId("reviews-heading")).toContainText("New");
+    // TODO: Add check for star icon
+  });
+
+  test("The review count on the spot's detail page should be an integer representing the total number of reviews for that spot followed by the text 'Reviews' (e.g. '4 Reviews')", async ({
+    page,
+  }) => {
+    await createSpotAndMultiReviews(page);
+    page.waitForTimeout(2000);
+    await expect(
+      page.getByTestId("spot-callout-box").getByTestId("review-count")
+    ).toContainText("2 Reviews");
+    await expect(
+      page.getByTestId("reviews-heading").getByTestId("review-count")
+    ).toContainText("2 Reviews");
+  });
+
+  test('If the review count is 1, it should show "1 Review" (not "1 Reviews").', async ({
+    page,
+  }) => {
+    await createSpotAndSingleReview(page);
+    page.waitForTimeout(2000);
+    await expect(
+      page.getByTestId("reviews-heading").getByTestId("review-count")
+    ).toContainText("1 Review");
+    await expect(
+      page.getByTestId("spot-callout-box").getByTestId("review-count")
+    ).toContainText("1 Review");
+  });
+
+  test("There should be a centered dot (·) between the star rating and the review count", async ({
+    page,
+  }) => {
+    await createSpotAndSingleReview(page);
+    page.waitForTimeout(2000);
+    await expect(page.getByTestId("reviews-heading")).toContainText(
+      /\d\.\d+\s*\d+\sReview/
+    );
+
+    await expect(page.getByTestId("spot-callout-box")).toContainText(
+      /\d\.\d+\s*\d+\sReview/
+    );
+  });
+
+  test('If the review count is zero (there are no reviews yet for this spot), it should not show the centered dot or the review count (only the star icon and "New" should be displayed)', async ({
+    page,
+  }) => {
+    const dummyData = createUniqueUser();
+    await encapsulateSpotCreation(page, dummyData);
+    page.waitForTimeout(2000);
+    await expect(
+      page.getByTestId("spot-callout-box").getByRole("paragraph")
+    ).not.toContainText("Review");
+    await expect(page.getByTestId("reviews-heading")).not.toContainText(
+      "Review"
+    );
+  });
+
+  test("Need a Test: When viewing the spot's detail page, show a list of the reviews for the spot below the spot's information with the newest reviews at the top, and the oldest reviews at the bottom.", async ({
+    page,
+  }) => {
+    const reviewsList = page.getByTestId("reviews-list");
+    const reviews = await reviewsList.getByTestId("review-item").all();
+
+    for (let i = 1; i < reviews.length; i++) {
+      const prevReviewDate = await reviews[i - 1]
+        .getByTestId("review-date")
+        .textContent();
+      const currReviewDate = await reviews[i]
+        .getByTestId("review-date")
+        .textContent();
+      expect(new Date(prevReviewDate!).getTime()).toBeGreaterThan(
+        new Date(currReviewDate!).getTime()
+      );
+    }
+  });
+
+  test("Each review in the review list must include: The reviewer's first name, the month and the year that the review was posted (e.g. December 2022), and the review comment text.", async ({
+    page,
+  }) => {
+    await createSpotAndSingleReview(page);
+    await page.getByTestId("review-list").first();
+
+    await expect(
+      page.getByTestId("review-list").first().getByText("Fakey")
+    ).toBeVisible();
+    await expect(
+      page.getByTestId("review-list").first().getByText("October")
+    ).toBeVisible();
+  });
+
+  test('If no reviews have been posted yet and the current user is logged-in and is NOT the owner of the spot, replace the reviews list with the text "Be the first to post a review!"', async ({
+    page,
+  }) => {
+    await createSpotAndNoReview(page);
+    await expect(
+      page.getByText("Be the first to post a review!")
+    ).toBeVisible();
+  });
+
+  test("The layout and element positioning is equivalent to the wireframes.", async ({
+    page,
+  }) => {
+    await createSpotAndSingleReview(page);
+    const spotInfo = await page.getByTestId("spot-location").boundingBox();
+    const reviewsSection = await page.getByTestId("review-list").boundingBox();
+    expect(reviewsSection?.y!).toBeGreaterThan(
+      spotInfo?.y! + spotInfo?.height!
+    );
+
+    const calloutBox = await page.getByTestId("spot-callout-box").boundingBox();
+    const viewportSize = page.viewportSize();
+    expect(calloutBox?.x! + calloutBox?.width!).toBeCloseTo(
+      viewportSize?.width!,
+      -3
+    );
+
+    const reviewHeading = await page
+      .getByTestId("reviews-heading")
+      .boundingBox();
+    const reviewsList = await page.getByTestId("review-list").boundingBox();
+    expect(reviewsList?.y!).toBeGreaterThan(
+      reviewHeading?.y! + reviewHeading?.height!
+    );
+  });
+});
--- tests/utils.ts
+++ tests/utils.ts
@@ -7,13 +7,111 @@ export function createUniqueUser() {
     password: "secret password",
   };
 }
-function generateUniqueUsername() {
+export function generateUniqueUsername() {
   const letters = Array.from({ length: 6 }, () =>
     String.fromCharCode(Math.floor(Math.random() * 26) + 97)
   ).join("");
   const numbers = Math.floor(Math.random() * 9000) + 1000;
   const timestamp = Date.now();
   return `${letters}${numbers}${timestamp}`;
+}
+
+export async function createSpotAndSingleReview(page){
+  const dummyData = createUniqueUser();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByText("Log In").click();
+  await page.getByRole("link", { name: "Log in as Demo User" }).click();
+  await page.getByRole("link", { name: "Create a New Spot" }).click();
+  await page.getByPlaceholder("Country").click();
+  await page.getByPlaceholder("Country").fill(`${dummyData.username} United`);
+  await page.getByPlaceholder("Street Address").click();
+  await page
+    .getByPlaceholder("Street Address")
+    .fill(`${dummyData.username} Lane`);
+  await page.getByPlaceholder("City").click();
+  await page.getByPlaceholder("City").fill(`Fort ${dummyData.username}`);
+  await page.getByPlaceholder("State").click();
+  await page.getByPlaceholder("State").fill(`New ${dummyData.username}`);
+  await page.getByPlaceholder("Please write at least 30").click();
+  await page
+    .getByPlaceholder("Please write at least 30")
+    .fill(
+      `Great Spot! ${dummyData.username} ${dummyData.username}. See you there!`
+    );
+  await page.getByPlaceholder("Name of your spot").click();
+  await page
+    .getByPlaceholder("Name of your spot")
+    .fill(`Cozy ${dummyData.username} Cabin`);
+  await page.getByPlaceholder("Price per night (USD)").click();
+  await page.getByPlaceholder("Price per night (USD)").fill("350");
+  await page.getByPlaceholder("Preview Image URL").click();
+  await page.getByPlaceholder("Preview Image URL").click();
+  await page
+    .getByPlaceholder("Preview Image URL")
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(1).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(1)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(2).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(2)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(3).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(3)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(4).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(4)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByRole("button", { name: "Create Spot" }).click();
+  await page.waitForTimeout(2000);
+  let url = page.url();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByRole("button", { name: "Log Out" }).click();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByText("Sign Up").click();
+
+  await page.getByTestId("first-name-input").click();
+  await page.getByTestId("first-name-input").fill(`Fakey ${dummyData.username}`);
+  await page.getByTestId("first-name-input").press("Tab");
+  await page.getByTestId("last-name-input").fill(dummyData.username);
+  await page.getByTestId("last-name-input").press("Tab");
+  await page
+    .getByTestId("email-input")
+    .fill(`${dummyData.username}@test.com`);
+  await page.getByTestId("email-input").press("Tab");
+  await page.getByTestId("username-input").fill(dummyData.username);
+  await page.getByTestId("username-input").press("Tab");
+  await page.getByTestId("password-input").fill("password");
+  await page.getByTestId("password-input").press("Tab");
+  await page.getByTestId("confirm-password-input").fill("password");
+  await page.getByTestId("form-sign-up-button").click();
+  await page.waitForTimeout(2000);
+  await page.goto(url);
+  await page.waitForTimeout(2000);
+  await page.getByTestId("review-button").click();
+  await page.getByPlaceholder("Leave your review here...").click();
+  await page
+    .getByPlaceholder("Leave your review here...")
+    .fill("Nice spot!!!!!!!!!");
+  await page.getByTestId("star-rating").locator("path").nth(4).click();
+  await page.getByRole("button", { name: "Submit Your Review" }).click();
 }
 
 
@@ -23,7 +121,7 @@ export async function signUpUser(page) {
   await page.getByTestId("user-menu-button").click();
   await page.getByText("Sign Up").click();
   await page.getByTestId("first-name-input").click();
-  await page.getByTestId("first-name-input").fill(user.firstName);
+  await page.getByTestId("first-name-input").fill(`Fakey ${user.username}`);
   await page.getByTestId("first-name-input").press("Tab");
   await page.getByTestId("last-name-input").fill(user.lastName);
   await page.getByTestId("last-name-input").press("Tab");
@@ -134,7 +232,7 @@ export async function clearSession(page) {
 }
 
 export async function createReviewByDemo(page) {
-  
+
   let reviewDetails = generateUniqueUsername()
   await page.getByTestId("review-button").click();
   await page
@@ -143,4 +241,235 @@ export async function createReviewByDemo(page) {
   await page.getByTestId("star-rating").locator("path").nth(4).click();
 
   await page.getByRole("button", { name: "Submit Your Review" }).click();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export async function encapsulateSpotCreation(page, dummyData){
+  await page.getByTestId("user-menu-button").click();
+  await page.getByText("Log In").click();
+  await page.getByRole("link", { name: "Log in as Demo User" }).click();
+  await page.getByRole("link", { name: "Create a New Spot" }).click();
+  await page.getByPlaceholder("Country").click();
+  await page.getByPlaceholder("Country").fill(`${dummyData.username} United`);
+  await page.getByPlaceholder("Street Address").click();
+  await page
+    .getByPlaceholder("Street Address")
+    .fill(`${dummyData.username} Lane`);
+  await page.getByPlaceholder("City").click();
+  await page.getByPlaceholder("City").fill(`Fort ${dummyData.username}`);
+  await page.getByPlaceholder("State").click();
+  await page.getByPlaceholder("State").fill(`New ${dummyData.username}`);
+  await page.getByPlaceholder("Please write at least 30").click();
+  await page
+    .getByPlaceholder("Please write at least 30")
+    .fill(
+      `Great Spot! ${dummyData.username} ${dummyData.username}. See you there!`
+    );
+  await page.getByPlaceholder("Name of your spot").click();
+  await page
+    .getByPlaceholder("Name of your spot")
+    .fill(`Cozy ${dummyData.username} Cabin`);
+  await page.getByPlaceholder("Price per night (USD)").click();
+  await page.getByPlaceholder("Price per night (USD)").fill("350");
+  await page.getByPlaceholder("Preview Image URL").click();
+  await page.getByPlaceholder("Preview Image URL").click();
+  await page
+    .getByPlaceholder("Preview Image URL")
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(1).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(1)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(2).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(2)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(3).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(3)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(4).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(4)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByRole("button", { name: "Create Spot" }).click();
+}
+
+
+
+export async function encapsulateUserCreation(page){
+  const dummyData = createUniqueUser();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByRole("button", { name: "Log Out" }).click();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByText("Sign Up").click();
+
+  await page.getByTestId("first-name-input").click();
+  await page.getByTestId("first-name-input").fill(dummyData.username);
+  await page.getByTestId("first-name-input").press("Tab");
+  await page.getByTestId("last-name-input").fill(dummyData.username);
+  await page.getByTestId("last-name-input").press("Tab");
+  await page
+    .getByTestId("email-input")
+    .fill(`${dummyData.username}@test.com`);
+  await page.getByTestId("email-input").press("Tab");
+  await page.getByTestId("username-input").fill(dummyData.username);
+  await page.getByTestId("username-input").press("Tab");
+  await page.getByTestId("password-input").fill("password");
+  await page.getByTestId("password-input").press("Tab");
+  await page.getByTestId("confirm-password-input").fill("password");
+  await page.getByTestId("form-sign-up-button").click()
+}
+
+export async function createSpotAndMultiReviews(page){
+
+  const dummyData2 = createUniqueUser();
+  await encapsulateSpotCreation(page, dummyData2)
+  await page.waitForTimeout(2000);
+  let url = page.url();
+  await encapsulateUserCreation(page)
+  await page.waitForTimeout(2000);
+  await page.goto(url);
+  await page.waitForTimeout(2000);
+  await page.getByTestId("review-button").click();
+  await page.getByPlaceholder("Leave your review here...").click();
+  await page
+    .getByPlaceholder("Leave your review here...")
+    .fill("Nice spot!!!!!!!!!");
+  await page.getByTestId("star-rating").locator("path").nth(4).click();
+  await page.getByRole("button", { name: "Submit Your Review" }).click();
+
+  await encapsulateUserCreation(page)
+  await page.waitForTimeout(2000);
+  await page.goto(url);
+  await page.waitForTimeout(2000);
+  await page.getByTestId("review-button").click();
+  await page.getByPlaceholder("Leave your review here...").click();
+  await page
+    .getByPlaceholder("Leave your review here...")
+    .fill("Nice spot!!!!!!!!!");
+  await page.getByTestId("star-rating").locator("path").nth(4).click();
+  await page.getByRole("button", { name: "Submit Your Review" }).click();
+}
+
+
+
+
+export async function createSpotAndNoReview(page){
+  const dummyData = createUniqueUser();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByText("Log In").click();
+  await page.getByRole("link", { name: "Log in as Demo User" }).click();
+  await page.getByRole("link", { name: "Create a New Spot" }).click();
+  await page.getByPlaceholder("Country").click();
+  await page.getByPlaceholder("Country").fill(`${dummyData.username} United`);
+  await page.getByPlaceholder("Street Address").click();
+  await page
+    .getByPlaceholder("Street Address")
+    .fill(`${dummyData.username} Lane`);
+  await page.getByPlaceholder("City").click();
+  await page.getByPlaceholder("City").fill(`Fort ${dummyData.username}`);
+  await page.getByPlaceholder("State").click();
+  await page.getByPlaceholder("State").fill(`New ${dummyData.username}`);
+  await page.getByPlaceholder("Please write at least 30").click();
+  await page
+    .getByPlaceholder("Please write at least 30")
+    .fill(
+      `Great Spot! ${dummyData.username} ${dummyData.username}. See you there!`
+    );
+  await page.getByPlaceholder("Name of your spot").click();
+  await page
+    .getByPlaceholder("Name of your spot")
+    .fill(`Cozy ${dummyData.username} Cabin`);
+  await page.getByPlaceholder("Price per night (USD)").click();
+  await page.getByPlaceholder("Price per night (USD)").fill("350");
+  await page.getByPlaceholder("Preview Image URL").click();
+  await page.getByPlaceholder("Preview Image URL").click();
+  await page
+    .getByPlaceholder("Preview Image URL")
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(1).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(1)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(2).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(2)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(3).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(3)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByPlaceholder("Image URL").nth(4).click();
+  await page
+    .getByPlaceholder("Image URL")
+    .nth(4)
+    .fill(
+      "https://img.freepik.com/free-photo/3d-house-model-with-modern-architecture_23-2151004049.jpg"
+    );
+  await page.getByRole("button", { name: "Create Spot" }).click();
+  await page.waitForTimeout(2000);
+  let url = page.url();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByRole("button", { name: "Log Out" }).click();
+  await page.getByTestId("user-menu-button").click();
+  await page.getByText("Sign Up").click();
+
+  await page.getByTestId("first-name-input").click();
+  await page.getByTestId("first-name-input").fill(`Fakey ${dummyData.username}`);
+  await page.getByTestId("first-name-input").press("Tab");
+  await page.getByTestId("last-name-input").fill(dummyData.username);
+  await page.getByTestId("last-name-input").press("Tab");
+  await page
+    .getByTestId("email-input")
+    .fill(`${dummyData.username}@test.com`);
+  await page.getByTestId("email-input").press("Tab");
+  await page.getByTestId("username-input").fill(dummyData.username);
+  await page.getByTestId("username-input").press("Tab");
+  await page.getByTestId("password-input").fill("password");
+  await page.getByTestId("password-input").press("Tab");
+  await page.getByTestId("confirm-password-input").fill("password");
+  await page.getByTestId("form-sign-up-button").click();
+  await page.waitForTimeout(2000);
+  await page.goto(url);
+  await page.waitForTimeout(2000);
+  await page.getByTestId("review-button").click();
 }
